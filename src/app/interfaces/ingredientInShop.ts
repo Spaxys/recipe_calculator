338c2a7ea7c0d15bc29bf342e31e636fadeb28ef
Daniel Webiörn <spaxys@live.se>
2018-06-25 18:38:57 +0200

    Improved style etc

---
+++ src/app/interfaces/ingredientInShop.ts
@@ -0,0 +1,11 @@
+export interface IIngredientInShop {
+    id: number;
+    ingredientId: number;
+    ingredientName: string;
+    shopId: number;
+    shopName: string;
+    name: string;
+    price: string;
+    unit: string;
+    quantity: string;
+}
